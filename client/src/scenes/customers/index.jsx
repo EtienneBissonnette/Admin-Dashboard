45c54a18f5c36db1ebc489be6e2cb9a8c9ddc9b3
Etienne <bissonnette@live.ca>
2023-06-23 15:51:23 -0400

    Customer page completed

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -29,7 +29,34 @@ const Customers = () => {
         title="CUSTOMERS"
         subtitle="Below is your list of customers"
       ></Header>
-      <Box>
+      <Box
+        marginTop="40px"
+        height="75vh"
+         sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
         <DataGrid
           loading={isLoading || !data}
           getRowId={(row) => row._id}
